dfadae6f191e54f42e72f6ba1a46c715546858a9
cjayswal <cjayswal@gmail.com>
2016-11-03 13:23:39 +0530

    added example project skeleton

---
+++ src/qaf/example/tests/SampleTest.java
@@ -0,0 +1,12 @@
+package qaf.example.tests;
+
+import org.testng.annotations.Test;
+
+import com.qmetry.qaf.automation.ui.WebDriverTestCase;
+
+public class SampleTest extends WebDriverTestCase {
+
+	@Test
+	public void testGoogleSearch() {
+	}
+}
